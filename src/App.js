6f9f6c3ad934551b3926f464e8d7dd44f4badb0f
Hugo Oswell <hugo.oswell@gmail.com>
2020-04-07 22:06:18 +0200

    New react app with new scripts versions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from "react";
-import Register from "./Pages/Register";
-import Login from "./Pages/Login";
-import Home from "./Pages/Home";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
