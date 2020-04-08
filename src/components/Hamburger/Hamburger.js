4811eed43f380ce85343d91662d7d401154ae117
Hugo Oswell <hugo.oswell@gmail.com>
2020-04-08 22:47:14 +0200

    Start to use state

--- src/components/Hamburger/Hamburger.js
+++ src/components/Hamburger/Hamburger.js
@@ -2,8 +2,12 @@ import React from "react";
 import "./Hamburger.css";
 
 const Hamburger = () => {
+  function openNavList() {
+    console.log("burger cliqué");
+  }
+
   return (
-    <div>
+    <div onClick={openNavList}>
       <span className="block h-05 w-8 dark-grey-bg rounded-sm"></span>
       <span className="block h-05 w-8 dark-grey-bg rounded-sm mt-2"></span>
       <span className="block h-05 w-8 dark-grey-bg rounded-sm mt-2"></span>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Hamburger from "../Hamburger/Hamburger";
 import Logo from "../Logos/Logo";
 import SubscribeCta from "../Ctas/SubscribeCta";
 import NavList from "./Nav/NavList";
+import "./Nav/NavList.css";
 
 const Header = () => {
+  const [isDeployed, SetIsDeployed] = useState(false);
+
   return (
-    <>
-      <div className="fixed top-0 w-full h-16 flex items-center justify-around">
+    <div className="h-16 fixed top-0 w-full bg-red-300">
+      <div className="h-16 flex items-center justify-around">
         <Hamburger />
         <Logo />
         <SubscribeCta isLoggedIn={false} />
       </div>
-      <div id="nav" className="mt-16 flex flex-col items-center">
+      <div id="navlist" className="flex flex-col items-center">
         <NavList isLoggedIn={false} />
       </div>
-    </>
+    </div>
   );
 };
 
