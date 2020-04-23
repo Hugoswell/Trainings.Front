57320f280c0798d40755749298833ea1a5ebd1f8
Hugo Oswell <hugo.oswell@gmail.com>
2020-04-23 22:58:44 +0200

    Finish SignIn page

--- src/Pages/SignIn.js
+++ src/Pages/SignIn.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../components/Header/Header";
+import SignInForm from "../components/Forms/SignInForm";
 
 const SignIn = () => {
   return (
     <>
       <Header />
+      <SignInForm />
     </>
   );
 };
