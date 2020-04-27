6cd4b8acfc6fded8b6cdeeb427cff55b4aee1384
Hugo Oswell <hugo.oswell@gmail.com>
2020-04-27 23:08:33 +0200

    Forward on featurees

--- src/components/Features/Features.js
+++ src/components/Features/Features.js
@@ -1,12 +1,14 @@
 import React from "react"
 import RunImg from "../../img/run.svg"
+import HeartImg from "../../img/heart.svg"
+import AnalyseImg from "../../img/analyse.svg"
 
 const Features = () => {
     return (
-        <div className="mt-6">
-            <img src={RunImg} alt="Running image" className="h-16"/>
-            <img src={RunImg} alt="Running image" className="h-16"/>
-            <img src="" alt=""/>
+        <div className="flex justify-between mt-6">
+            <img src={RunImg} alt="Running image" className="inline-block h-16"/>
+            <img src={HeartImg} alt="Running image" className="inline-block h-14 mt-8"/>
+            <img src={AnalyseImg} alt="" className="inline-block h-12 mt-4"/>
         </div>
     );
 }
