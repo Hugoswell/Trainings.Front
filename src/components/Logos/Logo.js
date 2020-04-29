acfac996376a7bf2097ef8afd39cec3d6756a472
Hugo Oswell <hugo.oswell@gmail.com>
2020-04-29 22:13:18 +0200

    Remove header router

--- src/components/Logos/Logo.js
+++ src/components/Logos/Logo.js
@@ -1,15 +1,13 @@
 import React from "react";
-import { BrowserRouter as Router, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import LogoImg from "../../img/logo.png";
 
 const Logo = () => {
   return (
     <div className="absolute absolute-center-horizontaly">
-      <Router>
-        <Link to="/">
-          <img src={LogoImg} alt="Trainings logo" className="h-12" />
-        </Link>
-      </Router>
+      <Link to="/">
+        <img src={LogoImg} alt="Trainings logo" className="h-12" />
+      </Link>
     </div>
   );
 };
