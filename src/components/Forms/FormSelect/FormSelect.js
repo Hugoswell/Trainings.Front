c31bcfaf3e05911b0d841f8849feda6060796bf6
Hugo Oswell <hugo.oswell@gmail.com>
2020-05-05 18:49:53 +0200

    Forward on select validation

--- src/components/Forms/FormSelect/FormSelect.js
+++ src/components/Forms/FormSelect/FormSelect.js
@@ -2,11 +2,19 @@ import React from 'react'
 
 const FormSelect = (props) => {
     return (
-        <select name="goal" id="goal" className="h-14 pl-6 mt-2 light-grey-bg dark-grey gotham-medium text-lg rounded-lg">
+        <select
+            id={props.name}
+            name={props.name}            
+            ref={props.register({
+                required: props.required
+              })}
+            className="h-14 pl-6 mt-2 light-grey-bg dark-grey gotham-medium text-lg rounded-lg"
+        >
+            <option value="">Sélectionner</option>
             <option value="1">Perte de poids</option>
-            <option value="1">Prise de poids</option>
-            <option value="1">Explosivité</option>
-            <option value="1">Force</option>
+            <option value="2">Prise de poids</option>
+            <option value="3">Explosivité</option>
+            <option value="4">Force</option>
         </select>
     )
 }
