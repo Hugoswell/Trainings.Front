3b634b36964a6d6d0a82c146e0713f9772afbad5
Hugo Oswell <hugo.oswell@gmail.com>
2020-05-07 00:18:02 +0200

    Forward on user form

--- src/components/Forms/UserForm/UserPhysicalForm.js
+++ src/components/Forms/UserForm/UserPhysicalForm.js
@@ -5,7 +5,6 @@ import { useForm } from "react-hook-form"
 import UpdateStore from "./UpdateStore"
 import UserFormStep from "./UserFormStep"
 import FormLabel from "../FormLabel/FormLabel"
-import FormSelect from "../FormSelect/FormSelect"
 import FormInput from "../FormInput/FormInput"
 
 const UserPreferencesForm = () => {
@@ -29,7 +28,7 @@ const UserPreferencesForm = () => {
             <UserFormStep stepNumber="2" text="Votre physique ?"/>
             <form onSubmit={handleSubmit(onSubmit)} className="mt-2 flex flex-col">
                 <FormLabel htmlFor="level" text="Votre niveau"/>
-                <FormSelect name="level" register={register} required="Veuillez choisir un niveau"/>
+                
                 <span className="mt-2 text-red-500 gotham-book">
                     {errors.level && errors.level.message}
                 </span>
@@ -46,19 +45,7 @@ const UserPreferencesForm = () => {
                 />
                 <span className="mt-2 text-red-500 gotham-book">
                     {errors.trainingType && errors.trainingType.message}
-                </span>
-
-                <FormLabel htmlFor="durationRange" text="Durée d’entrainement"/>
-                <FormSelect name="durationRange" register={register} required="Veuillez choisir une tranche de temps par entrainement"/>
-                <span className="mt-2 text-red-500 gotham-book">
-                    {errors.durationRange && errors.durationRange.message}
-                </span>
-
-                <FormLabel htmlFor="equipment" text="Votre matériel"/>
-                <FormSelect name="equipment" register={register} required="Veuillez choisir votre contexte d'entrainement"/>
-                <span className="mt-2 text-red-500 gotham-book">
-                    {errors.equipment && errors.equipment.message}
-                </span>
+                </span>                
 
                 <div className="flex justify-between">
                     <button onClick={handlePrevious} className="h-14 w-45 mt-8 mb-32 orange-bg rounded text-lg text-white text-center gotham-bold">
--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -4,7 +4,7 @@ import "./Loader.css"
 const Loader = (props) => {    
     return(
         <div className={
-            `${props.loading ? "flex" : "hidden"} items-center justify-center`}>
+            `${props.loading ? "flex" : "hidden"} items-center justify-center mt-2`}>
             <span className="loader loader-circle"></span>
         </div>
     );
