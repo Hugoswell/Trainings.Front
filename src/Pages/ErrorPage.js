7e4952ff2a0470d593b509615f540c939f76cc52
Hugo Oswell <hugo.oswell@gmail.com>
2020-05-16 22:25:02 +0200

    Add media query check for web version

---
+++ src/Pages/ErrorPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ErrorPage = () => {
+    return (
+        <div className="h-screen flex items-center justify-center container-85">
+            <p className="text-center text-lg orange gotham-medium">
+                Bonjour, la version web de notre site n'est pas encore disponible. Veuillez afficher notre site sur votre téléphone.
+            </p>
+        </div>
+    )
+}
+
+export default ErrorPage;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App/App";
 import * as serviceWorker from "./serviceWorker";
+import ErrorPage from "../src/Pages/ErrorPage"
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const ClientIsOnMobile = () => {
+    return window.screen.width <= 480;
+}
 
+{ ClientIsOnMobile() && ReactDOM.render(<App />, document.getElementById("root")); }
+{ !ClientIsOnMobile() && ReactDOM.render(<ErrorPage />, document.getElementById("root")); }
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.register();
+
+
