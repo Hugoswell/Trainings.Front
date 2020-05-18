bf6cae2fb98f387c9bd6d3942773ca16efa06219
Hugo Oswell <hugo.oswell@gmail.com>
2020-05-18 14:40:06 +0200

    Finish exercice training component

--- src/Pages/Training.js
+++ src/Pages/Training.js
@@ -8,6 +8,8 @@ import UrlBuilder from "../components/Helpers/UrlBuilder"
 import { useHistory } from "react-router-dom";
 import AuthContext from "../App/AuthContext"
 import Loader from "../components/Loader/Loader"
+import TrainingImage from "../components/Training/TrainingImage"
+import ExerciceTraining from "../components/Training/ExerciceTraining"
 
 const Training = () => {
     const [fetchedTraining, setFetchedTraining] = useState(null);    
@@ -28,22 +30,59 @@ const Training = () => {
 			Axios.get(getTrainingUrl, { headers: {  Authorization: `Bearer ${jwt}` } })
 				.then((response) => {
                     setFetchedTraining(response.data)
-					console.log(response.data);
 				})
 				.catch((error) => {
 					console.log(error.data);
 			});
 		}
     });
+
+    const setImgName = () => {
+        if (fetchedTraining) {
+            switch (fetchedTraining.trainingTypeName) {
+                case "Musculation":
+                    return "musculation-HF"
+                case "Cardio":
+                    return "cardio-F"
+                case "Crossfit":
+                    return "crossfit-H"
+                case "HIIT":
+                    return "crossfit-H"
+                default:
+                    break;
+            }
+        }
+    }
     
     if (fetchedTraining) {
         return (
             <>
                 <Header />
-                <div className="mt-24">
-                    Training number {id}
+                <TrainingImage 
+                    trainingTypeName={fetchedTraining.trainingTypeName}
+                    creationDate={fetchedTraining.creationDate}
+                    duration={fetchedTraining.duration}
+                    imgName={setImgName()} />
+                <div className="mt-4 container-85">
+                {
+                    (fetchedTraining.trainingTypeName ==="Crossfit"
+                    || fetchedTraining.trainingTypeName ==="HIIT") &&
+                    <p className="mt-8 text-center dark-grey gotham-medium text-xl">X {fetchedTraining.nbTimes}</p>
+                }
+                {
+                    fetchedTraining.exerciceTrainingViewModels.map(exercice =>
+                        <ExerciceTraining
+                            key={fetchedTraining.exerciceTrainingViewModels.indexOf(exercice)}
+                            trainingTypeName={fetchedTraining.trainingTypeName}
+                            exerciceName={exercice.exerciceName}
+                            restDuration={exercice.restDuration}
+                            effortDuration={exercice.effortDuration}
+                            nbSets={exercice.nbSets}
+                            nbReps={exercice.nbReps}
+                        />)
+                }
                 </div>
-                <div className="h-16"></div>
+                <div className="h-32"></div>
                 <BottomNav/>
             </>
         )
